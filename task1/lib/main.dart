973ddd5f735e3862f04d3d0056ecfd0e85371102
hagar110 <66685704+hagar110@users.noreply.github.com>
2021-04-08 14:38:04 +0200

    1st commit

---
+++ task1/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(        
+          margin: const EdgeInsets.only(left:30.0 , right: 30.0, top: 100.0 , bottom: 210.0),
+          decoration: BoxDecoration(
+                   boxShadow: [
+                      BoxShadow(
+                      color: Colors.blueGrey,
+                    offset: const Offset(5.0, 5.0),
+                    blurRadius: 20.0,
+                    spreadRadius: 5.0,
+                      )
+                    ],
+                    border: Border(
+                      top: BorderSide(width: 3.0,color: Colors.deepOrange,),
+                      left: BorderSide(width: 3.0,color: Colors.deepOrange,),
+                      right: BorderSide(width: 3.0,color: Colors.deepOrange,),
+                      bottom: BorderSide(width: 3.0,color: Colors.deepOrange,)
+                    ),
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(10.0), bottom: Radius.circular(10.0)),
+          ),
+          child: Center(
+             child: Column(
+             children: [
+                Container(
+                  width: 300.0,
+                  height: 200.0,
+                  decoration: BoxDecoration(
+                   
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(5.0)),
+                    image: DecorationImage(
+                        image: AssetImage(
+                          'assets/flutter5.png',
+                        ),
+                        fit: BoxFit.fill),
+                  ),
+                ),
+                Container(
+                  width: 300.0,
+                  height: 200.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.vertical(bottom: Radius.circular(5.0)),
+                    image: DecorationImage(
+                        image: AssetImage(
+                          'assets/flutter5.png',
+                        ),
+                        fit: BoxFit.fill),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
